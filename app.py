276bc9974c6dd20d6f6f7b47cf9117097b30994e
raven-hec <42874516+raven-hec@users.noreply.github.com>
2018-09-08 13:37:45 +0800

    Add get user by id method

--- app.py
+++ app.py
@@ -47,6 +47,28 @@ def list_users():
 	return jsonify({'user_list': api_list})
 
 
+@app.route("/api/v1/users/<int: user_id>", methods=["GET"])
+def get_user(user_id):
+	return list_user(user_id)
+
+def list_user(user_id)：
+	conn = sqlite3.connect("mydb.db")
+	print ("Opened database successfully");
+	api_list=[]
+	cursor = conn.cursor()
+	cursor.execute("SELECT * FROM users where id=?", (user_id))
+	data = cursor.fetchall()
+	if len(data) != 0:
+		user = {}
+		user['username'] = data[0][0]
+		user['name'] = data[0][1]
+		user['email'] = data[0][2]
+		user['password'] = data[0][3]
+		user['id'] = data[0][4]
+	conn.close()
+	return jsonify(a_dict)
+	
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000, debug=True)
 
